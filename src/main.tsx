c0f8d9a9a19446bd9db97fc5af5b3674b5f76d8f
kuttappu507 <smj.507@gmail.com>
2026-08-13 23:04:06 +0530

    Load Anek Malayalam font bundle

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import "@fontsource-variable/anek-malayalam/wght.css";
 import "./styles/globals.css";
+import "./styles/malayalam-font.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
